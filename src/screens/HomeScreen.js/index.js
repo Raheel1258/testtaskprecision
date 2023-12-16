fe224cf4625013d7f7293078e987c709f6d8a320
Rahil Ahmad <rahilahmad1258@gmail.com>
2023-12-16 12:01:00 +0500

    initalize project

---
+++ src/screens/HomeScreen.js/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid,GridItem } from "@chakra-ui/react";
+
+import { SideBar } from "../../components/SideBar";
+
+export const Home = () => {
+  return (
+    <Grid
+      templateAreas={`
+                  "nav subNav main"
+                 
+                  `}
+      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="100%"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"subNav"}>
+        SubNav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
+};
